c9937fbff749f76041ebf3dfd8f9b437cc48b5e8
rodrigodiaz <rodrighodiaz@gmail.com>
2021-10-02 11:58:14 -0500

    Initial commit

---
+++ app/src/main/java/com/hardcodeflow/eroticstories/SpikioApp.kt
@@ -0,0 +1,22 @@
+package com.hardcodeflow.eroticstories
+
+import android.app.Application
+
+class SpikioApp : Application(){
+
+
+    override fun onCreate() {
+        super.onCreate()
+       // var intent= Intent(baseContext, SplashActivity::class.java)
+        //refWatcher=LeakCanary.install(this)
+       /* Handler().postDelayed({
+            baseContext.startActivity(intent)
+            //baseContext.finish()
+        }, 2000)*/
+
+
+
+    }
+
+
+}
